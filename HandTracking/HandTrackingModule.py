1c3acbcf37fe0c9d8df4c9660eb5d03c1eb44f7a
Camilo Barreto <csbarreto11@gmail.com>
2024-09-19 16:39:54 +0100

    Changed color and size of center circle

--- HandTracking/HandTrackingModule.py
+++ HandTracking/HandTrackingModule.py
@@ -82,10 +82,10 @@ class handDetector():
         cx, cy = (x1 + x2)//2, (y1 + y2)//2
 
         if draw:
-            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
             cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), 10, (0, 0, 255), cv2.FILLED)
 
         length = math.hypot(x2-x1, y2-y1)
         return length, img, [x1, y1, x2, y2, cx, cy]
